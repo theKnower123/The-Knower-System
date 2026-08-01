1ed9ee057a22f0348f5d61b6731b6ddbd784d152
Omar Mehawed <71278779+omarmehawed@users.noreply.github.com>
2026-08-02 01:30:58 +0300

    Reorganize Vite config and restore build settings

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
-   
     plugins: [
         TanStackRouterVite({
             routesDirectory: './resources/js/routes',
             generatedRouteTree: './resources/js/routeTree.gen.ts',
         }),
-        ],
-        build: {
-        reportCompressedSize: false, // السطر ده هيحل مشكلة الرامات
-        chunkSizeWarningLimit: 2000,
-    },
         tailwindcss(),
         laravel({
             input: ['resources/js/app.tsx', 'resources/js/public_app.tsx'],
@@ -24,6 +17,10 @@ export default defineConfig({
         }),
         react(),
     ],
+    build: {
+        reportCompressedSize: false, // السطر ده هيحل مشكلة الرامات
+        chunkSizeWarningLimit: 2000,
+    },
     resolve: {
         alias: {
             '@': '/resources/js',
